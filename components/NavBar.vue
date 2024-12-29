96222f78c98a986d9eeedc49cc85eb8dd72a13e0
Sergiu Vidreanu <vidreanu97@gmail.com>
2024-12-29 17:42:53 +0100

    Open Source

---
+++ components/NavBar.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="py-4 mb-12 border-b border-solid border-gray-600">
+    <a
+      class="text-4xl prose prose-xl font-semibold text-green-500 letter-spacing"
+      href="/"
+      target="_self"
+      rel="noopener noreferrer"
+    >
+      f.a.r.t.s.
+    </a>
+  </div>
+</template>
